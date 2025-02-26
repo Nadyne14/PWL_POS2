e03f11f0c0df7206cba133b4c4a5fac1bec9b7e6
Nadyne14 <nadynerosalia1234@gmail.com>
2025-02-26 22:03:58 +0700

    11.	Laporkan hasil Praktikum-5

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -9,13 +9,27 @@ class KategoriController extends Controller
 {
     public function index() 
     {
-        $data = [
-            'kategori_kode' => 'SNK',
-            'nama_kategori' => 'Snack/Makanan Ringan',
-            'created_at' => now()
-        ];
+         // untuk menambahkan 1 data ke table m_kategori
+        // $data = [
+        //     'kategori_kode' => 'SNK',
+        //     'nama_kategori' => 'Snack/Makanan Ringan',
+        //     'created_at' => now()
+        // ];
 
-        DB::table('m_kategori')->insert($data);
-        return 'Insert data baru berhasil';
+        // DB::table('m_kategori')->insert($data);
+        // return 'Insert data baru berhasil';
+        
+        // untuk meng-update data di table m_kategori
+        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['nama_kategori' => 'Camilan']);
+        // return 'Update data berhasil. Jumlah data yang diupdate: ' . $row . ' baris';
+
+        // // hapus data
+        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
+        // return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row . ' baris';
+
+        // untuk menampilkan data yang ada di table m_kategori
+        $data = DB::table('m_kategori')->get();
+        return view('kategori', ['data' => $data]);
+
     }
 }
