ff084b872f46beb270b7acdd27a3facd8bb2549c
Nadyne14 <nadynerosalia1234@gmail.com>
2025-03-09 08:11:47 +0700

    UP SUDAH

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,21 +10,53 @@ class UserController extends Controller
 {
     public function index()
     {
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_tiga',
-        //     'nama' => 'Manager 3',
-        //     'password' => Hash::make('12345')
-        // ];
-        // UserModel::create($data);
-
-        // coba akses model UserModel
-        // $user = UserModel::findOrFail(1);
-        // return view('user', ['data' => $user]);
-
-        $user = UserModel::where('level_id', 2)->count();
-        // dd($user);
-        return view('user', ['data' => $user]);
+        $user = UserModel::with('level')->get();
+        return view('user', ['data' => $user]); 
     }
-}
 
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request)
+    {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make($request->password),
+            'level_id' => $request->level_id
+        ]);
+
+        return redirect('/user');
+    }
+
+    public function ubah($id)
+    {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_simpan($id, Request $request)
+    {
+        $user = UserModel::find($id);
+
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make($request->password);
+        $user->level_id = $request->level_id;
+
+        $user->save();
+
+        return redirect('/user');
+    }
+    
+    public function hapus($id)  
+    {  
+        $user = UserModel::find($id);  
+        $user->delete();  
+
+        return redirect('/user');  
+    }
+
+}
